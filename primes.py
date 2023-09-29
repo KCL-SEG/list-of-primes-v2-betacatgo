c43cdf45836fb146518ac5e79a0cea677833c4f0
Juntong Deng <juntong.deng@kcl.ac.uk>
2023-09-29 19:11:34 +0800

    Add list of primes

--- primes.py
+++ primes.py
@@ -1,6 +1,25 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
+import math
+
+def is_prime(num):
+    if num == 2:
+        return True
+    elif num % 2 == 0:
+        return False
+    for i in range(3, int(math.sqrt(num)) + 1, 2):
+        if num % i == 0:
+            return False
+    return True
 
 def primes(number_of_primes):
+    if number_of_primes < 1:
+        raise ValueError
+
     list = []
+    num = 2
+    while len(list) < number_of_primes:
+        if is_prime(num):
+            list.append(num)
+        num += 1
     return list
